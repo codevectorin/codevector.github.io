8ed626cdec7085642c13e4eb06fccb1cd2a50ff5
mobihack <aaduthoma19118@gmail.com>
2017-07-08 12:29:07 +0530

    Removed debug messages!

--- assets/js/bulma.js
+++ assets/js/bulma.js
@@ -3,8 +3,10 @@
 (function() {
     var burger = document.querySelector('.nav-toggle');
     var menu = document.querySelector('.nav-menu');
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
+    if (burger!=null) {
+        burger.addEventListener('click', function() {
+            burger.classList.toggle('is-active');
+            menu.classList.toggle('is-active');
+        });
+    }
 })();
